de94c22892ff1676616eb608ccc66c69469bdbe3
guilhermeSCampos <guilhermeregular@gmail.com>
2023-04-28 02:03:18 -0300

    feat: addHeroes script done

--- addHeroes.js
+++ addHeroes.js
@@ -1,6 +1,11 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 
+const fiveSkillHeroes = ['Beastmaster', 'Dark Willow',
+  'Earth Spirit', 'Ember Spirit', 'Ogre Magi', 'Techies', 'Timbersaw', 'Tinker', 'Tiny', 'Treant Protector', 'Troll Warlord']
+
+const sixSkillHeros = ['Io', 'Keeper of the Light', 'Morphling']
+
 const URL = 'https://www.dota2.com/hero/abaddon'
 
 const teste = async () => {
@@ -21,7 +26,7 @@ const teste = async () => {
     waitUntil: "networkidle2",
   });
 
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 124; i++) {
     let name = await page.evaluate(() => {
       return document.querySelector('.heropage_HeroName_2IcIu')?.innerHTML
     })
@@ -35,11 +40,54 @@ const teste = async () => {
       return document.querySelector('.heropage_Portrait_CR-Bb')?.src
     })
 
+    let type = await page.evaluate(() => {
+      return document.querySelector('.heropage_PrimaryStat_3HGWJ')?.innerHTML
+    })
+
+    let baseHp = await page.evaluate(() => {
+      return document.querySelector('.heropage_HealthBar_D6gmc')?.innerText.substring(0, 3)
+    })
+
+    let baseMp = await page.evaluate(() => {
+      return document.querySelector('.heropage_ManaBar_1aQk6')?.innerText.substring(0, 3)
+    })
+
+    let baseAttack = await page.evaluate(() => {
+      return document.querySelector('#dota_react_root > div > div > div.heropage_DetailsBarContainer_2v_HD > div > div.heropage_DetailsStats_22x6X > div.heropage_StatsList_3z1y6 > div:nth-child(1) > div:nth-child(2)')?.innerText
+    })
+
+    let baseDefense = await page.evaluate(() => {
+      return document.querySelector('#dota_react_root > div > div > div.heropage_DetailsBarContainer_2v_HD > div > div.heropage_DetailsStats_22x6X > div.heropage_StatsList_3z1y6 > div:nth-child(2) > div:nth-child(2)')?.innerText
+    })
+
+    let moveSpeed = await page.evaluate(() => {
+      return document.querySelector('#dota_react_root > div > div > div.heropage_DetailsBarContainer_2v_HD > div > div.heropage_DetailsStats_22x6X > div.heropage_StatsList_3z1y6 > div:nth-child(3) > div:nth-child(2)')?.innerText
+    })
+
     const skills = []
 
     const divChildren = await page.$('.heropage_AbilitySelector_1vjw5')
 
-    for (let i = 0; i < 4; i++) {
+    let numberOfSkills = 4
+    let initialSkill = 0;
+
+    if (fiveSkillHeroes.includes(name)) {
+      console.log('five skills hero')
+      numberOfSkills = 5
+    }
+
+    if (sixSkillHeros.includes(name)) {
+      console.log('six skills hero')
+      numberOfSkills = 6
+    }
+
+    if (name === 'Invoker') {
+      console.log('invoker')
+      initialSkill = 4;
+      numberOfSkills = 14
+    }
+
+    for (let i = initialSkill; i < numberOfSkills; i++) {
       const child = await divChildren.$(`div:nth-child(${i + 1})`)
       await child.click()
       let SkillImg = await page.evaluate(() => {
@@ -56,15 +104,21 @@ const teste = async () => {
       })
     }
 
+    console.log(name)
+
     const newHero = {
+      type,
       name,
       img,
+      baseHp,
+      baseMp,
+      baseAttack,
+      baseDefense,
+      moveSpeed,
       skills
     }
 
     heroes.push(newHero)
-
-    console.log(heroes)
 
     const nextHeroSelector = '#dota_react_root > div > div >' +
       'div.heropage_UnderBarSection_HGabF > div.heropage_BottomSection_kmUD- > a:nth-child(3)'
